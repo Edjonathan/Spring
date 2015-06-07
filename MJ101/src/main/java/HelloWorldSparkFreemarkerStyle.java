c33499c2937dcafadd341f4ad24486dd23b412cd
Guedes <Guedes@Guedes-PC>
2015-06-07 11:29:47 -0300

    .

---
+++ MJ101/src/main/java/HelloWorldSparkFreemarkerStyle.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+
+public class HelloWorldSparkFreemarkerStyle {
+
+	public static void main(String[] args) throws IOException {
+		
+		final Configuration configuration = new Configuration();
+		configuration.setClassForTemplateLoading(HelloWorldSparkFreemarkerStyle.class, "/");
+			
+		
+		Spark.get(new Route("/") {
+			
+			@Override
+			public Object handle(Request arg0, Response arg1) {
+				// TODO Auto-generated method stub
+				StringWriter writter  = new StringWriter();
+				try {
+					
+					Template helloTemplate = configuration.getTemplate("hello.ftl");
+					Map<String,Object>helloMap = new HashMap<String, Object>();
+					helloMap.put("name","Freemarker");
+					helloTemplate.process(helloMap, writter);
+				
+				} catch (Exception e) {
+					halt(500);
+					e.printStackTrace();
+				}		
+				return writter;
+			}
+		});
+	
+	}
+}
